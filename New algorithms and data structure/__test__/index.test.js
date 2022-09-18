7cf791137c82c9cc031a42474ba07759cb678dbf
okarachidera <okara.chidera@gmail.com>
2022-09-18 11:15:42 +0100

    feat: created test file

---
+++ New algorithms and data structure/__test__/index.test.js
@@ -0,0 +1,12 @@
+// require toWeirdCase()
+const toWeirdCase = require("../toWeirdCase");
+
+describe("toWeirdCase", () => {
+  it("should return the correct value for a single word", () => {
+    expect(toWeirdCase("This")).toBe("ThIs");
+    expect(toWeirdCase("is")).toBe("Is");
+  });
+  it("should return the correct value for multiple words", () => {
+    expect(toWeirdCase("This is a test")).toBe("ThIs Is A TeSt");
+  });
+});
